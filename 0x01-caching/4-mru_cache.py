bac68be4588cfa3d833a66dba58aca100fa8da61
Ahmedkel <ahmedkel@gmail.com>
2024-01-30 17:41:50 +0100

    MRU caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,1 +1,32 @@
 #!/usr/bin/env python3
+""" MRU Caching """
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ MRU Caching """
+
+    def __init__(self):
+        """ Constructor """
+        super().__init__()
+        self.mru_keys = []
+
+    def put(self, key, item):
+        """ Put Method """
+        if key and item:
+            if key in self.cache_data:
+                self.mru_keys.remove(key)
+            elif len(self.cache_data) >= self.MAX_ITEMS:
+                print("DISCARD: {}".format(self.mru_keys[-1]))
+                del self.cache_data[self.mru_keys[-1]]
+                self.mru_keys.pop()
+            self.mru_keys.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get Method """
+        if key in self.cache_data:
+            self.mru_keys.remove(key)
+            self.mru_keys.append(key)
+            return self.cache_data[key]
+        return None
