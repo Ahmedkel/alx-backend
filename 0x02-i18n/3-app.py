93954e3464f8636b814e88a0afd3a50b04ccccdc
Ahmedkel <ahmedkel@gmail.com>
2024-02-06 15:40:52 +0100

    more fixing

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -29,7 +29,7 @@ def get_index() -> str:
     Renders an HTML page
     with a title and a header.
     """
-    return render_template("2-index.html")
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
