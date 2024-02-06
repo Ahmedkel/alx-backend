40e250eda812f821ad4a306f2d1b7a83de7788d1
Ahmedkel <ahmedkel@gmail.com>
2024-02-06 14:45:02 +0100

    pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """
-    Renders an HTML page 
+    Renders an HTML page
     with a title and a header.
     """
     return render_template("0-index.html",)
